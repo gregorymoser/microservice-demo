1ed6ede7eb034c547af2fd4f88093c6fcfd54ee5
Gregory Moser <gdc.moser@hotmail.com>
2021-12-02 23:52:20 +0000

    User with Zuul config

--- hr-user/src/main/java/com/gm/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/gm/hruser/resources/UserResource.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gm.hruser.entities.User;
@@ -28,6 +29,12 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findByEmail(@RequestParam String email) {
+		User obj = repository.findByEmail(email);
 		return ResponseEntity.ok(obj);
 	}
 }
